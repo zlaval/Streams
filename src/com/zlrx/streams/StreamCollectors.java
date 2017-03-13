7aec627bbd160ed886e1171367126db8a886afb3
Zalan Toth <toth.zalan.ext@nokia.com>
2017-03-13 11:00:40 +0100

    collectors

--- src/com/zlrx/streams/StreamCollectors.java
+++ src/com/zlrx/streams/StreamCollectors.java
@@ -1,6 +1,7 @@
 package com.zlrx.streams;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,7 +10,15 @@ public class StreamCollectors {
 
     public static void main(String[] args) {
         StreamCollectors streamCollectors = new StreamCollectors();
-        streamCollectors.grouping();
+        streamCollectors.start();
+    }
+
+    private void start() {
+        grouping();
+        averageYear();
+        toMap();
+        partitioning();
+        ownCollector();
     }
 
     private void grouping() {
@@ -18,17 +27,50 @@ public class StreamCollectors {
         System.out.println(carsMappedByYear);
     }
 
+    private void partitioning() {
+        List<Car> cars = carsGenerator();
+        Map<Boolean, List<Car>> carsPartitionedByYear =
+                cars.stream().collect(Collectors.partitioningBy(car -> car.year > 2012));
+        System.out.println(carsPartitionedByYear);
+    }
+
+    private void averageYear() {
+        List<Car> cars = carsGenerator();
+        Double averageYear = cars.stream().collect(Collectors.averagingInt(car -> car.year));
+        System.out.println(averageYear);
+    }
+
+    private void toMap() {
+        List<Car> cars = carsGenerator();
+        Map<String, Integer> carNameYearMap = cars.stream().collect(Collectors.toMap(car -> car.name, car -> car.year));
+        System.out.println(carNameYearMap);
+    }
+
+    private void ownCollector() {
+        List<Car> cars = carsGenerator();
+
+        List<Car> carsAfter2013 = cars.stream()
+                .filter(car -> car.year > 2013)
+                .collect(
+                        LinkedList::new,
+                        LinkedList::add,
+                        LinkedList::addAll
+                );
+
+        System.out.println(carsAfter2013);
+    }
+
 
     private List<Car> carsGenerator() {
         List<Car> cars = new ArrayList<>(10);
         cars.add(new Car("Audi", 2015));
         cars.add(new Car("Ford", 2011));
-        cars.add(new Car("Wolksvagen", 2015));
+        cars.add(new Car("Volkswagen", 2015));
         cars.add(new Car("Bmw", 2013));
         cars.add(new Car("Lada", 1999));
         cars.add(new Car("Opel", 2015));
         cars.add(new Car("Seat", 2013));
-        cars.add(new Car("Lamorghini", 2017));
+        cars.add(new Car("Lamborghini", 2017));
         cars.add(new Car("Trabant", 2008));
         cars.add(new Car("Mercedes", 2011));
         return cars;
@@ -36,7 +78,7 @@ public class StreamCollectors {
 
     class Car {
         String name;
-        public Integer year;
+        Integer year;
 
         public Car(String name, Integer year) {
             this.name = name;
@@ -45,7 +87,7 @@ public class StreamCollectors {
 
         @Override
         public String toString() {
-            return String.format("%s márkájú autó, melyet %d-ben gyártottak", name, year);
+            return String.format("%s - %d", name, year);
         }
     }
 
